afbdaee09acb7779efc058736de6cae28a05992d
Santiago <00verasantiago@gmail.com>
2024-08-27 22:11:02 -0500

    creacion proyecto

---
+++ src/main/java/com/bootcamp_2024_1/Pokedex_To_Hexagonal/PokedexToHexagonalApplication.java
@@ -0,0 +1,13 @@
+package com.bootcamp_2024_1.Pokedex_To_Hexagonal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokedexToHexagonalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PokedexToHexagonalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bootcamp_2024_1/Pokedex_To_Hexagonal/PokedexToHexagonalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bootcamp_2024_1.Pokedex_To_Hexagonal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokedexToHexagonalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
